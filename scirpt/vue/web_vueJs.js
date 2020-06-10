48d32e4c281797978bebc5ef85b28e243079d7af
ZiMing <wwgg012345@gmail.com>
2020-06-11 02:11:43 +0800

    測試 商品圖片動畫

--- scirpt/vue/web_vueJs.js
+++ scirpt/vue/web_vueJs.js
@@ -9,18 +9,11 @@ var list = new Vue({
 		
 		show_shap:false,
 		
-		list:[
-			
-			{
-				id: '1',
-				name: 'glasses01_blue',
-				imgUrl:'http://127.0.0.1:8887/img/shap/glasses01-blue.png',
-				price:'1000',
-				info:'here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour',
-				count:'1',
-			},		
-			
-		],
+		name: 'glasses01_blue',
+		imgUrl:'http://127.0.0.1:8887/img/shap/glasses01-blue.png',
+		price:'1000',
+		info:'here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour',
+		count:'1',	
 		
 	},
 	
@@ -37,15 +30,21 @@ var list = new Vue({
 			
 			if(colorindex === 1) {
 				
-			document.querySelector('.glasses_list').setAttribute('style', 'background-color:#A8D5EA');
+				document.querySelector('.glasses_list').setAttribute('style', 'background-color:#A8D5EA');
+				this.imgUrl='http://127.0.0.1:8887/img/shap/glasses01-blue.png';
+				this.name='glasses01_blue';
 				
 			}else if(colorindex === 2){
 				
 				document.querySelector('.glasses_list').setAttribute('style', 'background-color:#EF7E7E');
+				this.imgUrl='http://127.0.0.1:8887/img/shap/glasses01-red.png';
+				this.name='glasses01_red';
 				
 			}else{
 				
 				document.querySelector('.glasses_list').setAttribute('style', 'background-color:#EDC692');
+				this.imgUrl='http://127.0.0.1:8887/img/shap/glasses01-yellow.png';
+				this.name='glasses01_yellow';
 			}
 			
 		}
