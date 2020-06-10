029bd2a2277aadead9eb6fca241612b12ef210da
ZiMing <wwgg012345@gmail.com>
2020-06-11 01:04:59 +0800

    完成 shap.html 的banner 商品頁面 vue 數組資料

--- scirpt/vue/web_vueJs.js
+++ scirpt/vue/web_vueJs.js
@@ -1,1 +1,56 @@
 // JavaScript Document
+
+
+var list = new Vue({
+	
+	el:'#shap',
+	
+	data:{
+		
+		show_shap:false,
+		
+		list:[
+			
+			{
+				id: '1',
+				name: 'glasses01_blue',
+				imgUrl:'http://127.0.0.1:8887/img/shap/glasses01-blue.png',
+				price:'1000',
+				info:'here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour',
+				count:'1',
+			},		
+			
+		],
+		
+	},
+	
+	methods:{
+		
+		show:function()
+		{
+			this.show_shap = !this.show_shap;
+		},
+		
+		change_color:function(color)
+		{
+			var colorindex = color;
+			
+			if(colorindex === 1) {
+				
+			document.querySelector('.glasses_list').setAttribute('style', 'background-color:#A8D5EA');
+				
+			}else if(colorindex === 2){
+				
+				document.querySelector('.glasses_list').setAttribute('style', 'background-color:#EF7E7E');
+				
+			}else{
+				
+				document.querySelector('.glasses_list').setAttribute('style', 'background-color:#EDC692');
+			}
+			
+		}
+		
+	},
+	
+	
+});
