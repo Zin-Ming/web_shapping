fb7c715d327a5a428fe64cfe8d551cbe62f27763
ZiMing <wwgg012345@gmail.com>
2020-06-12 02:18:42 +0800

    完成組件的切換 測試push到 v-for中 目前可行

--- scirpt/vue/web_vueJs.js
+++ scirpt/vue/web_vueJs.js
@@ -1,80 +1,56 @@
 // JavaScript Document
+	
 
-var tabs = [
-	
-	{ 
-		name: 'glasses01_blue',
-		count:'1',
-		
-		component:{
-			
-			template:'<div class="glasses_list blue"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-blue.png" alt=""></li><li> glasses01_blue </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li></ul><p><i class="fas fa-shopping-cart"></i></p></div>'
-			
-		},
-	},
-	
-	{ 
-		name: 'glasses01_red',
-		count:'1',	
-		
-		component:{
-			
-			template:'<div class="glasses_list red"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-red.png" alt=""></li><li> glasses01_red </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li></ul><p><i class="fas fa-shopping-cart"></i></p></div>'
-			
-		},
-	},
-	
-	{ 
-		name: 'glasses01_yellow',
-		count:'1',	
-		
-		component:{
-			
-			template:'<div class="glasses_list yellow"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-yellow.png" alt=""></li><li> glasses01_yellow </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li></ul><p><i class="fas fa-shopping-cart"></i></p></div>'
-			
-		},
-	}
-	
-	
-];
+var g_blue = Vue.component('g_blue',{
 
-var various = [
+		template:'<div class="glasses_list blue"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-blue.png" alt=""></li><li> g_blue </li><li>$ 1000 </li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><button @click="add()">Add_cart</button></li></ul></div>'
+			
+});
 	
-	{ 
-		name: 'variou_blue',
-		count:'1',
-		
-		component:{
-			
-			template:'<div class="glasses_list blue"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses02-blue.png" alt=""></li><li> variou_blue </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li></ul><p><i class="fas fa-shopping-cart"></i></p></div>'
-			
-		},
-	},
+var g_red = Vue.component('g_red',{
 	
-	{ 
-		name: 'variou_red',
-		count:'1',	
-		
-		component:{
+		template:'<div class="glasses_list red"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-red.png" alt=""></li><li> g_red </li><li>$ 1000 </li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><button @click="add()">Add_cart</button></li></ul></div>'
 			
-			template:'<div class="glasses_list red"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses02-red.png" alt=""></li><li> variou_red </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li></ul><p><i class="fas fa-shopping-cart"></i></p></div>'
-			
-		},
-	},
+});
 	
-	{ 
-		name: 'variou_yellow',
-		count:'1',	
-		
-		component:{
-			
-			template:'<div class="glasses_list yellow"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses02-yellow.png" alt=""></li><li> variou_yellow </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li></ul><p><i class="fas fa-shopping-cart"></i></p></div>'
-			
-		},
-	}
+var g_yellow = Vue.component('g_yellow',{
 	
-];
+		template:'<div class="glasses_list yellow"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-yellow.png" alt=""></li><li> glasses01_yellow </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><button @click="add()">Add_cart</button></li></ul></div>'
+});
+	
 
+	
+var v_blue = Vue.component('v_blue',{ 
+			
+		template:'<div class="glasses_list blue"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses02-blue.png" alt=""></li><li> variou_blue </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><button @click="add()">Add_cart</button></li></ul></div>'
+});
+	
+var v_red = Vue.component('v_red',{	
+			
+		template:'<div class="glasses_list red"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses02-red.png" alt=""></li><li> variou_red </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><button @click="add()">Add_cart</button></li></ul></div>'
+});
+	
+var v_yellow = Vue.component('v_yellow',{
+			
+		template:'<div class="glasses_list yellow"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses02-yellow.png" alt=""></li><li> variou_yellow </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><button @click="add()">Add_cart</button></li></ul></div>',
+	
+	methods:{
+		
+		add:function(){
+			
+			var item = {name:'yellow',price:1000};
+			
+			glasses01.list.push(item);
+
+			
+		}
+		
+		
+	},
+	
+	
+});
+	
 
 
 var glasses01 = new Vue({
@@ -83,31 +59,84 @@ var glasses01 = new Vue({
 	
 	data:{
 		
+		g_view:'g_blue',
+		
+		v_view:'v_blue',
+		
 		show_shap:false,
 		show_various_page:false,
 		
-		various:various,
-		varTab:various[0],
-		
-		tabs:tabs,
-		currentTab:tabs[0],
+		list:[],
 	},
 	
 	methods:{
 		
-		show:function()
-		{
+		show:function(){
+			
 			this.show_shap = !this.show_shap;
 		},	
 		
-		show_various:function()
-		{
+		show_various:function(){
+			
 			this.show_various_page = !this.show_various_page;
 		},	
 		
+		show_g:function(color){
+			
+			if(color === 1){
+			
+				this.g_view = 'g_blue';
+				
+			}else if(color === 2){
+				
+				this.g_view = 'g_red';
+				
+			}else{
+				
+				this.g_view = 'g_yellow';
+			}
+			
+		},
+		
+		show_v:function(index){
+			
+			if(index === 1){
+				
+				this.v_view = 'v_blue';
+				
+			}else if(index === 2){
+				
+				this.v_view = 'v_red';
+				
+			}else{
+				
+				this.v_view = 'v_yellow';
+				
+			}
+			
+		}
+	
+	},
+	
+	component:{
+		
+		'g_blue':g_blue,
+		
+		'g_red':g_red,
+		
+		'g_yellow':g_yellow,
+		
+		'v_blue':v_blue,
+		
+		'v_red':v_red,
+		
+		'v_yellow':v_yellow,
+		
+
 	},
 	
 });
+
 
 
 
