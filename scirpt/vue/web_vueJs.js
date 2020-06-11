3f5c3a84ef22e464079e98d000ef68ac7bc1250f
ZiMing <wwgg012345@gmail.com>
2020-06-11 17:07:26 +0800

    完成 glasses01商品的組件化與動畫

--- scirpt/vue/web_vueJs.js
+++ scirpt/vue/web_vueJs.js
@@ -4,42 +4,33 @@ var tabs = [
 	
 	{ 
 		name: 'glasses01_blue',
-		imgUrl:'http://127.0.0.1:8887/img/shap/glasses01-blue.png',
-		price:'1000',
-		info:'here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour',
 		count:'1',
 		
 		component:{
 			
-			template:'<div class="glasses_list"><ul><li><img v-bind:src="imgUrl" alt=""></li><li>{{ name }}</li><li>${{ price }}</li><li>{{ info }}</li></ul><p><i class="fas fa-shopping-cart"></i></p></div>'
+			template:'<div class="glasses_list blue"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-blue.png" alt=""></li><li> glasses01_blue </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li></ul><p><i class="fas fa-shopping-cart"></i></p></div>'
 			
 		},
 	},
 	
 	{ 
 		name: 'glasses01_red',
-		imgUrl:'http://127.0.0.1:8887/img/shap/glasses01-red.png',
-		price:'1000',
-		info:'here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour',
 		count:'1',	
 		
 		component:{
 			
-			template:'<div class="glasses_list"><ul><li><img v-bind:src="imgUrl" alt=""></li><li>{{ name }}</li><li>${{ price }}</li><li>{{ info }}</li></ul><p><i class="fas fa-shopping-cart"></i></p></div>'
+			template:'<div class="glasses_list red"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-red.png" alt=""></li><li> glasses01_red </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li></ul><p><i class="fas fa-shopping-cart"></i></p></div>'
 			
 		},
 	},
 	
 	{ 
 		name: 'glasses01_yellow',
-		imgUrl:'http://127.0.0.1:8887/img/shap/glasses01-yellow.png',
-		price:'1000',
-		info:'here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour',
 		count:'1',	
 		
 		component:{
 			
-			template:'<div class="glasses_list"><ul><li><img v-bind:src="imgUrl" alt=""></li><li>{{ name }}</li><li>${{ price }}</li><li>{{ info }}</li></ul><p><i class="fas fa-shopping-cart"></i></p></div>'
+			template:'<div class="glasses_list yellow"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-yellow.png" alt=""></li><li> glasses01_yellow </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li></ul><p><i class="fas fa-shopping-cart"></i></p></div>'
 			
 		},
 	}
@@ -65,32 +56,7 @@ var list = new Vue({
 		show:function()
 		{
 			this.show_shap = !this.show_shap;
-		},
-		
-		change_color:function(color)
-		{
-			var colorindex = color;
-			
-			if(colorindex === 1) {
-				
-				document.querySelector('.glasses_list').setAttribute('style', 'background-color:#A8D5EA');
-				this.imgUrl='http://127.0.0.1:8887/img/shap/glasses01-blue.png';
-				this.name='glasses01_blue';
-				
-			}else if(colorindex === 2){
-				
-				document.querySelector('.glasses_list').setAttribute('style', 'background-color:#EF7E7E');
-				this.imgUrl='http://127.0.0.1:8887/img/shap/glasses01-red.png';
-				this.name='glasses01_red';
-				
-			}else{
-				
-				document.querySelector('.glasses_list').setAttribute('style', 'background-color:#EDC692');
-				this.imgUrl='http://127.0.0.1:8887/img/shap/glasses01-yellow.png';
-				this.name='glasses01_yellow';
-			}
-			
-		}
+		},	
 		
 	},
 	
