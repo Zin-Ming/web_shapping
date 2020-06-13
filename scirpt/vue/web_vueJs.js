760548026d3e11fc6a58bcc034f63765ebb0a43c
ZiMing <wwgg012345@gmail.com>
2020-06-14 02:11:22 +0800

    完成 所有頁面RWD 表單驗證

--- scirpt/vue/web_vueJs.js
+++ scirpt/vue/web_vueJs.js
@@ -3,7 +3,7 @@
 
 var g_blue = Vue.component('g_blue',{
 
-	template:'<div class="glasses_list blue"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-blue.png" alt=""></li><li> Ipsum_blue </li><li>$ 1000 </li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><div class="btn_cart" @click="add()">Add cart</div></li></ul></div>',
+	template:'<div class="glasses_list blue"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-blue.png" alt=""></li><li> Ipsum_blue </li><li>$ 1000 </li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><div class="add_cart" @click="add()">Add cart</div></li></ul></div>',
 	
 	methods:{
 		
@@ -40,7 +40,7 @@ var g_blue = Vue.component('g_blue',{
 	
 var g_red = Vue.component('g_red',{
 	
-	template:'<div class="glasses_list red"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-red.png" alt=""></li><li> Ipsum_red </li><li>$ 1000 </li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><div class="btn_cart" @click="add()">Add cart</div></li></ul></div>',
+	template:'<div class="glasses_list red"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-red.png" alt=""></li><li> Ipsum_red </li><li>$ 1000 </li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><div class="add_cart" @click="add()">Add cart</div></li></ul></div>',
 	
 	methods:{
 		
@@ -78,7 +78,7 @@ var g_red = Vue.component('g_red',{
 	
 var g_yellow = Vue.component('g_yellow',{
 	
-	template:'<div class="glasses_list yellow"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-yellow.png" alt=""></li><li> Ipsum_yellow </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><div class="btn_cart" @click="add()">Add cart</div></li></ul></div>',
+	template:'<div class="glasses_list yellow"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses01-yellow.png" alt=""></li><li> Ipsum_yellow </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><div class="add_cart" @click="add()">Add cart</div></li></ul></div>',
 	
 	methods:{
 		
@@ -119,7 +119,7 @@ var g_yellow = Vue.component('g_yellow',{
 	
 var v_blue = Vue.component('v_blue',{ 
 			
-	template:'<div class="glasses_list blue"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses02-blue.png" alt=""></li><li> variou_blue </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><div class="btn_cart" @click="add()">Add cart</div></li></ul></div>',
+	template:'<div class="glasses_list blue"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses02-blue.png" alt=""></li><li> variou_blue </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><div class="add_cart" @click="add()">Add cart</div></li></ul></div>',
 	
 	methods:{
 		
@@ -157,7 +157,7 @@ var v_blue = Vue.component('v_blue',{
 	
 var v_red = Vue.component('v_red',{	
 			
-	template:'<div class="glasses_list red"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses02-red.png" alt=""></li><li> variou_red </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><div class="btn_cart" @click="add()">Add cart</div></li></ul></div>',
+	template:'<div class="glasses_list red"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses02-red.png" alt=""></li><li> variou_red </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><div class="add_cart" @click="add()">Add cart</div></li></ul></div>',
 	
 	methods:{
 		
@@ -194,7 +194,7 @@ var v_red = Vue.component('v_red',{
 	
 var v_yellow = Vue.component('v_yellow',{
 			
-	template:'<div class="glasses_list yellow"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses02-yellow.png" alt=""></li><li> variou_yellow </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><div  class="btn_cart" @click="add()">Add cart</div></li></ul></div>',
+	template:'<div class="glasses_list yellow"><ul><li><img src="http://127.0.0.1:8887/img/shap/glasses02-yellow.png" alt=""></li><li> variou_yellow </li><li>$1000</li><li>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</li><li><div class="add_cart" @click="add()">Add cart</div></li></ul></div>',
 	
 	methods:{
 		
@@ -403,6 +403,214 @@ var glasses01 = new Vue({
 		
 
 	},
+	
+});
+
+
+var nav_menu = new Vue({
+	
+	el:'#nav',
+	
+	data:{
+		
+		isActive:false,
+		btn_menu:true,
+		btn_menu_x:false,
+
+	},
+	
+	methods:{
+		
+		btn_menu_show:function(){
+			
+			this.isActive = !this.isActive;
+			this.btn_menu = !this.btn_menu;
+			this.btn_menu_x = !this.btn_menu_x;
+		},
+		
+		/*btn_menu_show_x:function(){
+			
+			this.isActive = false;
+			this.btn_menu = true;
+			this.btn_menu_x = false;
+			
+		},*/
+		
+		
+	},
+	
+	
+});
+
+
+var contact = new Vue({
+	
+	el:'#contact',
+	
+	data:{
+		
+		name_error:false,
+		email_error:false,
+		number_error:false,
+		
+		name_err_msg_show:false,
+		e_err_msg_show:false,
+		number_err_msg_show:false,
+				
+		name_err_msg:'',
+		email_err_msg:'',
+		number_err_msg:'',
+		
+		name:'',
+		
+		number:'',
+		
+		email:'',
+			
+		msg:'',
+			
+		
+	},
+	
+	watch:{
+		
+		name:function(){
+			
+			var text = /^[a-zA-Z0-9]+$/;
+			
+			if(this.name.length > 10 ){
+				
+				this.name_error = true;
+				
+				this.name_err_msg_show = true;
+				
+				this.name_err_msg = '請勿超過10個字';
+				
+				
+			}else if(!text.test(this.name)){
+				
+				this.name_error = true;
+				
+				this.name_err_msg_show = true;
+				
+				this.name_err_msg = '請勿包含特殊字元或空白';
+				
+				
+			}else if(this.name === ''){
+				
+				this.name_error = true;
+
+				
+			}else{
+				
+				this.name_error = false;
+				
+				this.name_err_msg_show = false;
+				
+			}
+			
+		},
+		
+		number:function(){
+			
+			var n_text = /^[0-9]*$/;
+			
+			if(this.number.length > 11 || this.number.length < 10){
+				
+				this.number_error = true;
+				
+				this.number_err_msg_show = true;
+					
+				this.number_err_msg = '請輸入 10 - 11 個字';
+				
+			}else if(!n_text.test(this.number)){
+				
+				this.number_error = true;
+				
+				this.number_msg_show = true;
+					
+				this.number_err_msg = '請勿包含數字以外字元';
+				
+			}else if(this.number === ''){
+				
+				this.number_error = true;
+				
+				this.number_err_msg_show = true;
+					
+				this.number_err_msg = '請勿空白';
+				
+			}else{
+				
+				this.number_error = false;
+				
+				this.number_err_msg_show = false;
+				
+			}
+			
+		},
+		
+		email:function(){
+			
+			var e_text = /^\w+((-\w+)|(\.\w+)|(\+\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
+			
+				
+			if(!e_text.test(this.email)){
+				
+				this.email_error = true;
+				
+				this.e_err_msg_show = true;
+					
+				this.email_err_msg = '請輸入電子信箱格式';
+				
+			}else if(this.email === ''){
+				
+				this.email_error = true;
+				
+				this.e_err_msg_show = true;
+					
+				this.email_err_msg = '請勿空白';
+				
+			}else{
+				
+				this.email_error = false;
+				
+				this.e_err_msg_show = false;
+				
+			}
+			
+		}
+		
+		
+		
+		
+	},
+	
+	
+	methods:{
+		
+		submit:function(){
+			
+			if(this.name_error == true || this.number_error == true || this.email_error == true){
+			   
+			    alert('輸入資訊不正確');
+			   
+			}else if(this.name === '' || this.number === '' || this.email === ''){
+				
+				alert('輸入欄請勿空白');
+			
+			}else{
+			   
+				var user = { "name":this.name ,"email":this.email , "number":this.number  , "msg":this.msg };
+			
+				var userobj = JSON.stringify(user);
+			
+				console.log(userobj);
+			 }
+			
+		},
+		
+	}
+	
 	
 });
 
